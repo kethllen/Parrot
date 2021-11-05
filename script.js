d9cfd5f6228d85a2628ecf156ea8e4906c51e851
kethllen Andrey <kety_andrey@hotmail.com>
2021-11-04 22:20:59 -0300

    cartas embaralhadas

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let numeroDeCartas = 0;
+let qntPares = 0;
+let baralhoAtual = [];
 
 while(numeroDeCartas < 4 || numeroDeCartas > 14 || numeroDeCartas % 2 !==0){
    numeroDeCartas = parseInt(prompt("Com quantas cartas você quer jogar?(Escolha um número entre 4 e 14 cartas e que seja par)"));
@@ -6,10 +8,18 @@ while(numeroDeCartas < 4 || numeroDeCartas > 14 || numeroDeCartas % 2 !==0){
 
 const baralho = ["./assets/bobrossparrot.gif", "./assets/explodyparrot.gif", "./assets/fiestaparrot.gif","./assets/metalparrot.gif","./assets/revertitparrot.gif","./assets/tripletsparrot.gif","./assets/unicornparrot.gif"]
 
-baralho.sort(comparador); // Após esta linha, a minhaArray estará embaralhada
+ // Após esta linha, a minhaArray estará embaralhada
 // Esta função pode ficar separada do código acima, onde você preferir
 function comparador() { 
 	return Math.random() - 0.5; 
+}
+
+function embaralhar(){
+    baralho.sort(comparador);
+    qntPares = numeroDeCartas/2;
+    baralhoAtual = baralho.slice(0, qntPares);
+    baralhoAtual = baralhoAtual.concat(baralhoAtual);
+    baralhoAtual.sort(comparador);
 }
 function virarCartaBaixo(carta){
     const card = carta.parentNode;
@@ -28,8 +38,9 @@ function virarCartaCima(carta){
 
 function darCartas(){
     const card = document.querySelector("main");
-    for(let i=0; i<numeroDeCartas; i++){
 
+    for(let i=0; i<numeroDeCartas; i++){
+        embaralhar();
         card.innerHTML = card.innerHTML +  `
         <div class="card" data-identifier="card">
             <div class="front-face face" data-identifier="front-face" onclick="virarCartaBaixo(this)" >
@@ -37,7 +48,7 @@ function darCartas(){
                 </div>
             </div>
             <div class="back-face face " data-identifier="back-face" onclick="virarCartaCima(this)">
-                <img class="imagem" src="./assets/bobrossparrot.gif">
+                <img class="imagem" src=${baralhoAtual[i]}>
             </div>
         </div>`;
     }
